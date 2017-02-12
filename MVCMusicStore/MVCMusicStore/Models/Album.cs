b70c3f318e4e1d148f2911402c2f6cebbaad8e66
meijii <heyoones@gmail.com>
2017-02-12 14:07:18 +1300

    Trying to add Review as a Class to follow the tutorial but Visual Studio 2015 is not showing it

---
+++ MVCMusicStore/MVCMusicStore/Models/Album.cs
@@ -0,0 +1,13 @@
+﻿namespace MVCMusicStore.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+
+        public string Title { get; set; }
+
+        public Artist Artist { get; set; }
+
+        public Review
+    }
+}
---
+++ MVCMusicStore/MVCMusicStore/Models/Artist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStore.Models
+{
+    public class Artist
+    {
+        public int ArtistID { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Album> Albums { get; set; }
+
+    }
+
+}
